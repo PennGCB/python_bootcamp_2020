34f897eb1e6a4978cd0ee5ec3b1f64efae4b6584
William Bone <wbone@pennmedicine.upenn.edu>
2020-07-01 14:36:51 -0400

    Add HW1 files

---
+++ HW_files/HW_week_1/rock_paper_scissors_HW1_template.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python2.7
+
+#The goal of this script is to take in the files that have the list of choices made by player 1 and player 2 in their 100 games of rock, paper, scissors and return a file that has the result of each of the 100 games. please make sure the output file is called "" a list of game results that are "\n" delimited.
+
+#We have provided you the input files for player 1 and player 2 on the course Github account and we would like you to use the functions in this file to complete this coding assignment.
+
+#The player choices are in the form of a string for each round of "R", "P", or "S" (rock, paper, or scissors) separated by "\n"
+
+#The file2List function is designed to take the data files from player 1 and player 2 and convert them to a list data structure.
+def file2List():
+
+    pass
+
+#The compareChoices function is designed to take in a choice string from player 1 and a choice string from player 2 and assess what the result of each game was.
+#If player 1 won it should return the string "P1"
+#If player 2 won it shold return the string "P2"
+#If the players tied it should return the string "tie"
+def compareChoices():
+
+    pass
+
+#You need a main function for all python scripts please use the main function to organized the other functions of this script.
+def main():
+
+    pass
+
+if __name__ == "__main__":
+    main()
